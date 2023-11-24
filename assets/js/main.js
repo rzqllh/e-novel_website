c284d096b94f40cede80ef73b48ef6e55caff990
rzqllh <hrizqullah484@gmail.com>
2023-11-25 00:35:40 +0700

    Rampung

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,6 +199,8 @@ const sr = ScrollReveal({
   //reset: true, //animation repeat
 })
 
-sr.reveal('.home__data')
+sr.reveal(".home__data, .featured__container, .new__container, .join__data, .testimonial__container");
 sr.reveal('.home__images', {delay: 600})
 sr.reveal('.services__card', {interval: 100})
+sr.reveal('.discount__data', {origin: 'left'})
+sr.reveal('.discount__images', { origin: 'right'})
