0597634d0496e45b082d29331f33e8c26a2ddbf1
rzqllh <hrizqullah484@gmail.com>
2023-11-24 19:29:50 +0700

    ScrollReveal Error

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,12 +53,12 @@ if (loginClose) {
 
 
 /*=============== HOME SWIPER ===============*/
-let swiperHome = new Swiper('.home__swiper', {
+let swiperHome = new Swiper(".home__swiper", {
   loop: true,
-  spacebetween: -24,
+  spaceBetween: -24,
   grabCursor: true,
-  slidesPerView: 'auto',
-  centeredSlides: 'auto',
+  slidesPerView: "auto",
+  centeredSlides: "auto",
 
   autoplay: {
     delay: 3000,
@@ -67,22 +67,22 @@ let swiperHome = new Swiper('.home__swiper', {
 
   breakpoints: {
     1220: {
-      spaceBetween: -32
-    }
-  }
-})
+      spaceBetween: -32,
+    },
+  },
+});
 
 /*=============== FEATURED SWIPER ===============*/
 let swiperFeatured = new Swiper(".featured__swiper", {
   loop: true,
-  spacebetween: 16,
+  spaceBetween: 16,
   grabCursor: true,
   slidesPerView: "auto",
   centeredSlides: "auto",
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   breakpoints: {
@@ -96,7 +96,7 @@ let swiperFeatured = new Swiper(".featured__swiper", {
 /*=============== NEW SWIPER ===============*/
 let swiperNew = new Swiper(".new__swiper", {
   loop: true,
-  spacebetween: 16,
+  spaceBetween: 16,
   slidesPerView: "auto",
 
   breakpoints: {
@@ -107,15 +107,98 @@ let swiperNew = new Swiper(".new__swiper", {
 });
 
 /*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestimonial = new Swiper(".testimonial__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
 
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+  },
+});
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
 
-/*=============== DARK LIGHT THEME ===============*/ 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
 
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const sunIcon = "fa-sun";
+const moonIcon = "fa-moon";
+
+// Previously selected theme (if the user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+
+// We validate if the user previously chose a theme
+if (selectedTheme) {
+  // If the validation is fulfilled, set the theme
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  // Set the initial icon based on the current theme
+  themeButton.classList.toggle(moonIcon, selectedTheme !== "dark");
+  themeButton.classList.toggle(sunIcon, selectedTheme === "dark");
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Toggle the dark theme
+  document.body.classList.toggle(darkTheme);
+
+  // Toggle the icon based on the current theme
+  themeButton.classList.toggle(moonIcon, getCurrentTheme() !== "dark");
+  themeButton.classList.toggle(sunIcon, getCurrentTheme() === "dark");
+
+  // Save the theme
+  localStorage.setItem("selected-theme", getCurrentTheme());
+});
+
+/*=============== SCROLL REVEAL ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400, 
+  //reset: true, //animation repeat
+})
+
+sr.reveal('.home__data')
+sr.reveal('.home__images', {delay: 600})
+sr.reveal('.services__card', {interval: 100})
